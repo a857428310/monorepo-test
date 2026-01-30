e8cd74f155ea5bfd3f1eb14842a75d12e1091e0d
a857428310 <857428310@qq.com>
2026-01-30 16:00:14 +0800

    fix 删除无用代码

--- apps/web/src/views/login/index.vue
+++ apps/web/src/views/login/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
     <router-link :to="{ name: 'Dashboard' }">Dashboard</router-link>
-     <!-- 自定义标签名（默认为 <a>） -->
-    <router-link to="/dashboard" custom v-slot="{ navigate }">
-      <button @click="navigate">跳转到控制台</button>
-    </router-link>
   </div>
 </template>
